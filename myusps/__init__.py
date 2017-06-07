470389e3b5c6bb4b2b692199b493f62a8be10b59
happyleaves <happyleaves.tfr@gmail.com>
2017-06-07 15:29:11 -0400

    update for gtm; fix edited names

--- myusps/__init__.py
+++ myusps/__init__.py
@@ -1,14 +1,16 @@
 """My USPS interface."""
 
 import datetime
+import logging
 import os.path
 import pickle
+import re
 from bs4 import BeautifulSoup
 from dateutil.parser import parse
 import requests
 from requests.auth import AuthBase
 
-
+_LOGGER = logging.getLogger(__name__)
 HTML_PARSER = 'html.parser'
 LOGIN_FORM_TAG = 'form'
 LOGIN_FORM_ATTRS = {'name': 'loginForm'}
@@ -30,15 +32,15 @@ DATE_TAG = 'div'
 DATE_ATTRS = {'class': 'mypost-tracked-item-details-date'}
 STATUS_TAG = 'span'
 STATUS_ATTRS = {'class': 'mypost-tracked-item-details-status'}
-TRACKING_NUMBER_TAG = 'h2'
-TRACKING_NUMBER_ATTRS = {'class': ['mobile-status-green', 'mobile-status-blue',
-                                   'mobile-status-red']}
+TRACKING_NUMBER_TAG = 'div'
+TRACKING_NUMBER_ATTRS = {'style': 'word-break: break-word;'}
 ERROR_TAG = 'span'
 ERROR_ATTRS = {'class': 'error'}
 
-MY_USPS_URL = 'https://reg.usps.com/login?app=MyUSPS'
-AUTHENTICATE_URL = 'https://reg.usps.com/entreg/json/AuthenticateAction'
-LOGIN_URL = 'https://reg.usps.com/entreg/LoginAction'
+BASE_URL = 'https://reg.usps.com'
+MY_USPS_URL = BASE_URL + '/login?app=MyUSPS'
+AUTHENTICATE_URL = BASE_URL + '/entreg/json/AuthenticateAction'
+LOGIN_URL = BASE_URL + '/entreg/LoginAction'
 DASHBOARD_URL = 'https://my.usps.com/mobileWeb/pages/myusps/HomeAction_input'
 INFORMED_DELIVERY_URL = 'https://informeddelivery.usps.com/box/pages/secure/HomeAction_input.action'
 INFORMED_DELIVERY_IMAGE_URL = 'https://informeddelivery.usps.com/box/pages/secure/'
@@ -48,6 +50,9 @@ COOKIE_PATH = './usps_cookies.pickle'
 ATTRIBUTION = 'Information provided by www.usps.com'
 USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) ' \
              'Chrome/41.0.2228.0 Safari/537.36'
+EFMJ_HEADER = 'x-efmj'
+UUID_TOKEN_REGEX = re.compile(r'"uniqueStateKey",100,"(.+?)"', re.MULTILINE)
+PAYLOAD_KEY_REGEX = re.compile(r'httpMethods:\["POST"\]\}\],"(.+?)"', re.MULTILINE)
 
 
 class USPSError(Exception):
@@ -115,33 +120,49 @@ def _get_date(row):
 
 def _get_tracking_number(row):
     """Get package tracking number."""
-    return row.find(TRACKING_NUMBER_TAG, TRACKING_NUMBER_ATTRS).string.strip()
+    return row.find(TRACKING_NUMBER_TAG, TRACKING_NUMBER_ATTRS).string.replace(' ', '').strip()
 
 
-def _get_token(session):
-    """Get login token."""
-    form = _get_elem(session.get(MY_USPS_URL), LOGIN_FORM_TAG, LOGIN_FORM_ATTRS)
+def _get_login_metadata(session):
+    """Get login metadata."""
+    resp = session.get(MY_USPS_URL)
+    # Token for login form submission
+    form = _get_elem(resp, LOGIN_FORM_TAG, LOGIN_FORM_ATTRS)
     token_elem = form.find('input', {'name': 'token'})
-    if token_elem:
-        return token_elem.get('value')
-    raise USPSError('No login token found')
+    # UUID token and payload key for GTM compatibility
+    uuid_token_result = UUID_TOKEN_REGEX.search(resp.text)
+    payload_key_result = PAYLOAD_KEY_REGEX.search(resp.text)
+    if token_elem and uuid_token_result and payload_key_result:
+        _LOGGER.debug('login form token: %s', token_elem.get('value'))
+        _LOGGER.debug('gtm uuid token: %s', uuid_token_result.group(1))
+        _LOGGER.debug('gtm payload key: %s', payload_key_result.group(1))
+        return token_elem.get('value'), uuid_token_result.group(1), payload_key_result.group(1)
+    raise USPSError('No login metadata found')
 
 
 def _login(session):
     """Login."""
-    token = _get_token(session)
+    _LOGGER.debug("attempting login")
+    token, uuid_token, payload_key = _get_login_metadata(session)
     resp = session.post(AUTHENTICATE_URL, {
         'username':  session.auth.username,
         'password': session.auth.password
+    }, headers={
+        EFMJ_HEADER+'uniqueStateKey': uuid_token,
+        EFMJ_HEADER+payload_key: ''
     })
-    if resp.json()['rs'] != 'success':
+    data = resp.json()
+    if 'rs' not in data:
         raise USPSError('authentication failed')
-    error = _get_elem(session.post(LOGIN_URL, {
+    if data['rs'] != 'success':
+        raise USPSError('authentication failed')
+    resp = session.post(LOGIN_URL, {
         'username': session.auth.username,
         'password': session.auth.password,
         'token': token,
         'struts.token.name': 'token'
-    }), ERROR_TAG, ERROR_ATTRS)
+    })
+    error = _get_elem(resp, ERROR_TAG, ERROR_ATTRS)
     if error is not None:
         raise USPSError(error.text.strip())
     _save_cookies(session.cookies, session.auth.cookie_path)
@@ -154,6 +175,7 @@ def authenticated(function):
         try:
             return function(*args)
         except USPSError:
+            _LOGGER.info("attempted to access page before login")
             _login(args[0])
             return function(*args)
     return wrapped
@@ -176,6 +198,7 @@ def get_profile(session):
 @authenticated
 def get_packages(session):
     """Get package data."""
+    _LOGGER.info("attempting to get package data")
     packages = []
     response = session.get(DASHBOARD_URL)
     no_packages = _get_elem(response, NO_PACKAGES_TAG, NO_PACKAGES_ATTRS)
@@ -198,6 +221,7 @@ def get_packages(session):
 @authenticated
 def get_mail(session, date=datetime.datetime.now().date()):
     """Get mail data."""
+    _LOGGER.info("attempting to get informed delivery data")
     mail = []
     response = session.post(INFORMED_DELIVERY_URL, {
         'selectedDate': '{0:%m}/{0:%d}/{0:%Y}'.format(date)
@@ -232,6 +256,7 @@ def get_session(username, password, cookie_path=COOKIE_PATH):
     session.auth = USPSAuth(username, password, cookie_path)
     session.headers.update({'User-Agent': USER_AGENT})
     if os.path.exists(cookie_path):
+        _LOGGER.debug("cookie found at: %s", cookie_path)
         session.cookies = _load_cookies(cookie_path)
     else:
         _login(session)
